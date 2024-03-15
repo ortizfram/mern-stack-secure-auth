542f933169218e1f6b2427ad6c08c998fc6412e2
Franco Ortiz <ortizfranco48@yahoo.com>
2024-03-15 10:28:08 -0300

    add client register functionality

---
+++ client/pages/Register.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function Register() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordVerify, setPasswordVerify] = useState("");
+
+  async function register(e) {
+    e.preventDefault();
+    try {
+      const registerData = { email,username, password, passwordVerify };
+      await axios.post("http://localhost:2020/api/auth/signup", registerData);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  return (
+    <div>
+      <h1>Register</h1>
+      <form onSubmit={register}>
+        <input
+          type="text"
+          placeholder="username"
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <input
+          type="password"
+          placeholder="repeat password"
+          onChange={(e) => setPasswordVerify(e.target.value)}
+          value={passwordVerify}
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "../components/Navbar";
+import Navbar from "../layouts/Navbar";
+import Register from "../pages/Register";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
     <Navbar />
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/register" element={<h1>register</h1>} />
+        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<h1>login</h1>} />
         <Route path="/customer" element={<h1>customer</h1>} />
       </Routes>
